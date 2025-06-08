cf5e3535736abdabd158dd29c27978022cae578e
MrSanketPrajapatissp <160329456+MrSanketPrajapatissp@users.noreply.github.com>
2025-06-08 10:45:03 +0530

    Time: 20 ms (16.33%), Space: 49.3 MB (7.66%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,5 +1,5 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
+   /* public List<List<String>> groupAnagrams(String[] strs) {
       //  Time	O(m × n log n)
       //   Space	O(m × n)
         
@@ -16,5 +16,33 @@ class Solution {
 
        return new ArrayList<>(res.values());
 
+    }
+   */ 
+
+    public List<List<String>> groupAnagrams(String strs[])
+    {
+        Map <String, List<String>> res = new HashMap<>();
+        
+        // ✅ Time Complexity: O(m × n) – correct.
+        // ✅ Space Complexity:
+        // O(m) extra space for keys and HashMap
+        // O(m × n) space for output
+
+        for(String s : strs)
+        {
+            char[] charArray = s.toCharArray();
+            int count[] = new int[26];
+
+            for(char c: charArray)
+            {
+                count[c - 'a']++;
+            }
+
+            String key = Arrays.toString(count); //  key eg: "[1, 2, 0, 0, 0]"
+            res.putIfAbsent(key,new ArrayList<>());
+            res.get(key).add(s);
+        }
+
+        return new ArrayList<>(res.values());
     }
 }
